2ebe328f4f16f7b788c141a282f5f11233321ad8
Yuta OHWATARI <y.ohwatari@cartahd.com>
2022-12-07 16:33:46 +0900

    fix

--- src/message/chat.ts
+++ src/message/chat.ts
@@ -3,7 +3,7 @@ import {noBotMessages} from "../misc";
 import {ChatGPTAPI} from "chatgpt";
 
 export function listen(app: App, token?: string) {
-    app.message(/^!chat\s(.*)/gm, noBotMessages(), async ({context, say}) => {
+    app.message(/^!chat\s(.*)/, noBotMessages(), async ({context, say}) => {
         const api = new ChatGPTAPI({
             sessionToken: token as string,
             markdown: true,
