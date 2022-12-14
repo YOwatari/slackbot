17601dd0c1df3067c5d98d5c173ce8c3d0d16b2a
Yuta OHWATARI <y.ohwatari@cartahd.com>
2022-12-14 14:16:20 +0900

    no unfold url

--- src/message/chat.ts
+++ src/message/chat.ts
@@ -3,6 +3,9 @@ import {noBotMessages} from "../misc";
 
 export function listen(app: App, token?: string) {
     app.message(/^!chat\s(.*)/, noBotMessages(), async ({context, say}) => {
-        await say("対応停止中: https://github.com/YOwatari/slackbot/issues/12");
+        await say({
+            text: "対応停止中: https://github.com/YOwatari/slackbot/issues/12",
+            unfurl_links: false,
+        });
     });
 }
