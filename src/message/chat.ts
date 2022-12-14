298cff4d788e3d8f2765d3357b9df86b3588b345
Yuta OHWATARI <y.ohwatari@cartahd.com>
2022-12-15 02:57:15 +0900

    fix

--- src/message/chat.ts
+++ src/message/chat.ts
@@ -1,12 +1,12 @@
-import type {App} from "@slack/bolt";
-import {noBotMessages} from "../misc.js";
+import type { App } from '@slack/bolt'
+import { noBotMessages } from '../misc.js'
 
 export function listen(app: App, email?: string, password?: string) {
-    app.message(/^!chat\s(.*)/, noBotMessages(), async ({context, say}) => {
-        await say({
-            text: "対応停止中: https://github.com/YOwatari/slackbot/issues/12",
-            unfurl_links: false,
-            unfurl_media: false,
-        });
-    });
+  app.message(/^!chat\s(.*)/, noBotMessages(), async ({ context, say }) => {
+    await say({
+      text: '対応停止中: https://github.com/YOwatari/slackbot/issues/12',
+      unfurl_links: false,
+      unfurl_media: false,
+    })
+  })
 }
