b1f3ecef768ae45c4db1e321bdc02c4941107b19
yowatari <4982161+YOwatari@users.noreply.github.com>
2022-12-14 14:07:57 +0900

    disable chatgpt (#13)

--- src/message/chat.ts
+++ src/message/chat.ts
@@ -1,18 +1,8 @@
 import type {App} from "@slack/bolt";
 import {noBotMessages} from "../misc";
-import {ChatGPTAPI} from "chatgpt";
 
 export function listen(app: App, token?: string) {
     app.message(/^!chat\s(.*)/, noBotMessages(), async ({context, say}) => {
-        const api = new ChatGPTAPI({
-            sessionToken: token as string,
-            markdown: true,
-        });
-        await api.ensureAuth();
-
-        const prompt = context["matches"][1];
-        const response = await api.sendMessage(prompt);
-
-        await say(response);
+        await say("対応停止中: https://github.com/YOwatari/slackbot/issues/12");
     });
 }
