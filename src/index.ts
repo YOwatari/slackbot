b8ba95caa7a99573833812aa752101c4b1064d9b
Yuta OHWATARI <y.ohwatari@cartahd.com>
2022-12-15 02:42:38 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
-import slack from "@slack/bolt";
-const { App, LogLevel } = slack;
+import slack from '@slack/bolt'
 
-import { listen as jpi } from "./message/jpi.js";
-import { listen as erande } from "./message/erande.js";
-import { listen as chat } from "./message/chat.js";
+const { App, LogLevel } = slack
+
+import { listen as jpi } from './message/jpi.js'
+import { listen as erande } from './message/erande.js'
+import { listen as chat } from './message/chat.js'
 
 const app = new App({
-    logLevel: LogLevel.DEBUG,
-    token: process.env["SLACK_BOT_TOKEN"],
-    signingSecret: process.env["SLACK_SIGNING_SECRET"],
-    appToken: process.env["SLACK_APP_TOKEN"],
-    socketMode: true,
-});
+  logLevel: LogLevel.DEBUG,
+  token: process.env['SLACK_BOT_TOKEN'],
+  signingSecret: process.env['SLACK_SIGNING_SECRET'],
+  appToken: process.env['SLACK_APP_TOKEN'],
+  socketMode: true,
+})
 
 jpi(
-    app,
-    process.env["GOOGLE_API_KEY"],
-    process.env["GOOGLE_CUSTOM_SEARCH_ENGINE_ID"]
-);
-erande(app);
-chat(app, process.env["CHATGPT_SESSION_TOKEN"]);
+  app,
+  process.env['GOOGLE_API_KEY'],
+  process.env['GOOGLE_CUSTOM_SEARCH_ENGINE_ID']
+)
+erande(app)
+chat(app, process.env['OPENAI_EMAIL'], process.env['OPENAI_PASSWORD'])
 
-(async () => {
-    await app.start(Number(process.env["PORT"]) || 3000);
-    console.log("Bolt app is running");
-})();
+;(async () => {
+  await app.start(Number(process.env['PORT']) || 3000)
+  console.log('Bolt app is running')
+})()
--- src/misc.ts
+++ src/misc.ts
@@ -1,13 +1,11 @@
-import type {Middleware, SlackEventMiddlewareArgs} from "@slack/bolt";
+import type { Middleware, SlackEventMiddlewareArgs } from '@slack/bolt'
 
-export function noBotMessages(): Middleware<SlackEventMiddlewareArgs<'message'>> {
-    return async ({message, next}) => {
-        if (next === undefined) {
-            return;
-        }
-
-        if (!message.subtype || message.subtype != "bot_message") {
-            await next();
-        }
-    };
+export function noBotMessages(): Middleware<
+  SlackEventMiddlewareArgs<'message'>
+> {
+  return async ({ message, next }) => {
+    if (!message.subtype || message.subtype != 'bot_message') {
+      await next()
+    }
+  }
 }
