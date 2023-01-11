8176f2bc9304161d39462b780e56c7c30412734f
Yuta OHWATARI <y.ohwatari@cartahd.com>
2023-01-11 10:07:04 +0900

    nochatgpt mode

--- Makefile
+++ Makefile
@@ -6,6 +6,9 @@ all: build run
 run: stop
 	docker run -d --restart=always --env DISPLAY=host.docker.internal:0.0 --env-file $(CURDIR)/.env --name $(CONTAINER) $(IMAGE)
 
+run_slim: stop
+	docker run -d --restart=always --env DISPLAY=host.docker.internal:0.0 --env-file $(CURDIR)/.env --name $(CONTAINER) $(IMAGE) start -- --nochatgpt
+
 build:
 	docker buildx build -t $(IMAGE) --load .
 
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
-import slack from '@slack/bolt'
+// @ts-ignore
+import { parseArgs } from 'node:util'
 
+import slack from '@slack/bolt'
 const { App } = slack
+
 import { ChatGPTAPIBrowser } from 'chatgpt'
 
 import { listen as jpi } from './message/jpi.js'
 import { listen as erande } from './message/erande.js'
 import { listen as chat, listenUnavailable as nochat } from './message/chat.js'
+
+const {
+  values: { nochatgpt },
+} = parseArgs({
+  options: {
+    nochatgpt: {
+      type: 'boolean',
+      multiple: false,
+      default: false,
+    },
+  },
+  args: process.argv.slice(2),
+})
 
 const app = new App({
   token: process.env['SLACK_BOT_TOKEN'],
@@ -14,17 +30,20 @@ const app = new App({
   appToken: process.env['SLACK_APP_TOKEN'],
 })
 
-// const chatgpt = new ChatGPTAPIBrowser({
-//   email: String(process.env['OPENAI_EMAIL'] || ''),
-//   password: String(process.env['OPENAI_PASSWORD'] || ''),
-// })
-
 ;(async () => {
   jpi(app, process.env['GOOGLE_API_KEY'], process.env['GOOGLE_CUSTOM_SEARCH_ENGINE_ID'])
   erande(app)
-  // chat(app, chatgpt)
-  // await chatgpt.initSession()
-  nochat(app)
+
+  if (nochatgpt) {
+    nochat(app)
+  } else {
+    const chatgpt = new ChatGPTAPIBrowser({
+      email: String(process.env['OPENAI_EMAIL'] || ''),
+      password: String(process.env['OPENAI_PASSWORD'] || ''),
+    })
+    chat(app, chatgpt)
+    await chatgpt.initSession()
+  }
 
   await app.start(Number(process.env['PORT']) || 3000)
   console.log('Bolt app is running')
