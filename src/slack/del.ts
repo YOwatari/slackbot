30d91851f43ab07c09e1b282bc64d89d4a62e808
Yuta OHWATARI <y.ohwatari@cartahd.com>
2024-04-16 16:39:41 +0900

    style(slack/del.ts): standardize quotes and indentation for consistency refactor(slack/del.ts): improve timestamp parsing in parse function for clarity

--- src/slack/del.ts
+++ src/slack/del.ts
@@ -1,30 +1,31 @@
-import { SlackApp, SlackOAuthApp } from "slack-cloudflare-workers"
-import { NoBotMessage } from "./util"
+import { SlackApp, SlackOAuthApp } from 'slack-cloudflare-workers'
+import { NoBotMessage } from './util'
 
 export function del(app: SlackApp<any> | SlackOAuthApp<any>) {
-    const pattern = /^!del\s(.*)/
-    app.message(pattern, async ({ context, payload }) => {
-        if (NoBotMessage(payload)) {
-            const match = payload.text.match(pattern)
-            if (match && match[1]) {
-                await context.client.chat.delete({
-                    channel: payload.channel,
-                    ts: payload.ts
-                })
-            }
-        }
-    })
+  const pattern = /^!del\s(.*)/
+  app.message(pattern, async ({ context, payload }) => {
+    if (NoBotMessage(payload)) {
+      const match = payload.text.match(pattern)
+      if (match && match[1]) {
+        await context.client.chat.delete({
+          channel: payload.channel,
+          ts: payload.ts,
+        })
+      }
+    }
+  })
 }
 
-export function parse(url: string): { channel: string, ts: string } | null {
-    const pattern = /https:\/\/[\w-]+\.slack\.com\/archives\/([\w-]+)\/p(\d+)/
-    let match = url.match(pattern)
+export function parse(url: string): { channel: string; ts: string } | null {
+  const pattern = /https:\/\/[\w-]+\.slack\.com\/archives\/([\w-]+)\/p(\d+)/
+  let match = url.match(pattern)
 
-    if (match) {
-        const channel = match[1];
-        const ts = match[2];
-        return { channel, ts };
-    }
+  if (match) {
+    const channel = match[1]
+    const index = match[2].length - 6
+    const ts = `${match[2].slice(0, index)}.${match[2].slice(index)}`
+    return { channel, ts }
+  }
 
-  return null;
+  return null
 }
