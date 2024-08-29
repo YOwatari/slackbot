e80fcb4351e8f15a3b2767cb8fc2b78552efe9b6
Yuta OHWATARI <y.ohwatari@cartahd.com>
2024-08-29 16:20:12 +0900

    style(ping.ts): standardize import quotes from double to single for consistency style(ping.ts): format code for better readability by adjusting indentation and spacing

--- src/slack/ping.ts
+++ src/slack/ping.ts
@@ -1,13 +1,13 @@
-import { SlackApp, SlackOAuthApp } from "slack-cloudflare-workers"
-import { DirectMention, NoBotMessage } from "./util"
+import { SlackApp, SlackOAuthApp } from 'slack-cloudflare-workers'
+import { DirectMention, NoBotMessage } from './util'
 
 export function ping(app: SlackApp<any> | SlackOAuthApp<any>) {
   let pattern = /ping/
   app.message(pattern, async ({ context, payload }) => {
     if (NoBotMessage(payload) && DirectMention(context, payload)) {
-        await context.say({
-            text: `pong`,
-        })
+      await context.say({
+        text: `pong`,
+      })
     }
   })
 }
